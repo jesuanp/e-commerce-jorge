89f2b3236177707e21226462a357193ee209bd4f
jesuanp <jespaterm@gmail.com>
2023-02-06 10:44:28 -0400

    cambiando las imagenes de posicion

--- src/components/productos/Productos.js
+++ src/components/productos/Productos.js
@@ -21,6 +21,23 @@ export default function Productos() {
                 </div>
 
                 <div className={s.containerImg}>
+                    <strong className={s.textImage}>
+                        <span>NUEVOS ESTILOS DE</span>
+                        <br/>
+                        <span style={{fontSize: '4rem'}}>TENIS</span>
+                    </strong>
+
+                    <img className={s.imgShoes} src={img4} alt='sneakers new' />
+                    <NavLink to='/zapato'>
+                        <button style={{top: '7rem', bottom: 'auto'}} className={`${s.btnVerMas} ${s.absoliteLeft}`}>VER MÁS</button>
+                    </NavLink>
+                </div>
+                
+            </div>
+
+            <div className={s.section}>
+
+                <div className={s.containerImg}>
                     <div className={s.containerImg}>
                         <img className={s.imgShoes} src={img14} alt='image 13 zapatos' />
                         <NavLink to='/zapato'>
@@ -32,22 +49,6 @@ export default function Productos() {
                         <br/>
                         <span style={{fontSize: '1.2rem', color: 'rgb(201, 131, 0)'}}>152.900 COP</span>
                     </div>
-                </div>
-                
-            </div>
-
-            <div className={s.section}>
-                <div className={s.containerImg}>
-                    <strong className={s.textImage}>
-                        <span>NUEVOS ESTILOS DE</span>
-                        <br/>
-                        <span style={{fontSize: '4rem'}}>TENIS</span>
-                    </strong>
-
-                    <img className={s.imgShoes} src={img4} alt='sneakers new' />
-                    <NavLink to='/zapato'>
-                        <button style={{top: '7rem', bottom: 'auto'}} className={`${s.btnVerMas} ${s.absoliteLeft}`}>VER MÁS</button>
-                    </NavLink>
                 </div>
 
                 <div className={s.containerImg}>
